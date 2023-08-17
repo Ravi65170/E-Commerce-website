5822bb83afc34a72dd3157916d0b3d82e4032261
Ravi Prakash Yadav <Ravi65710@gmail.com>
2023-08-17 13:45:02 +0530

    sign in & log in page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 // import { Counter } from "./features/counter/Counter";
 // import "./App.css";
 import ProductList from "./features/product-list/ProductList";
-import Home from "./pages/Home";
+import Home from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      {/* <Home></Home> */}
+      {/* <LoginPage></LoginPage> */}
+      <SignupPage></SignupPage>
     </div>
   );
 }
